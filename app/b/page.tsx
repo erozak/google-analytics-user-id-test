4b31a46cf853f0e630987921444240a19feb0303
Erozak <pinex666@gmail.com>
2023-11-28 14:30:43 +0800

    feat: add pages

--- app/b/page.tsx
+++ app/b/page.tsx
@@ -1,12 +1,12 @@
-import Link from 'next/Link';
+import Link from 'next/link';
 
-export default function Home() {
+export default function BPage() {
   return (
     <main className='p-8'>
-      <h1 className='text-3xl mb-8'>Homepage</h1>
+      <h1 className='text-3xl mb-8'>B Page</h1>
       <nav className='flex flex-col space-y-4 my-4'>
+        <Link href="/">Go to Homepage</Link>
         <Link href="/a">Go to A page</Link>
-        <Link href="/b">Go to B page</Link>
       </nav>
     </main>
   )
