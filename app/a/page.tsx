e6573f60e0d75c9d757a7ee835963e97035af648
Erozak <pinex666@gmail.com>
2023-11-30 12:24:00 +0800

    feat: revise page headers

--- app/a/page.tsx
+++ app/a/page.tsx
@@ -1,12 +1,17 @@
+import type { Metadata } from 'next';
 import Link from 'next/link';
+
+export const metadata: Metadata = {
+  title: 'Page A',
+}
 
 export default function APage() {
   return (
     <main className='p-8'>
-      <h1 className='text-3xl mb-8'>A Page</h1>
+      <h1 className='text-3xl mb-8'>Page A</h1>
       <nav className='flex flex-col space-y-4 my-4'>
         <Link href="/">Go to Homepage</Link>
-        <Link href="/b">Go to B page</Link>
+        <Link href="/b">Go to page B</Link>
       </nav>
     </main>
   )
--- app/b/page.tsx
+++ app/b/page.tsx
@@ -1,12 +1,17 @@
+import type { Metadata } from 'next';
 import Link from 'next/link';
+
+export const metadata: Metadata = {
+  title: 'Page B',
+}
 
 export default function BPage() {
   return (
     <main className='p-8'>
-      <h1 className='text-3xl mb-8'>B Page</h1>
+      <h1 className='text-3xl mb-8'>Page B</h1>
       <nav className='flex flex-col space-y-4 my-4'>
         <Link href="/">Go to Homepage</Link>
-        <Link href="/a">Go to A page</Link>
+        <Link href="/a">Go to page A</Link>
       </nav>
     </main>
   )
